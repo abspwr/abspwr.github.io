ceb03660ba46f25406990eb74a2228ae92b4ff2f
abspwr <abspwrba@gmail.com>
2022-05-02 21:14:56 +0200

    license_checker_3 crackme and passcode generator

---
+++ re/unix/licesnse_checker_3/license_checker_3-key-generator-brute.go
@@ -0,0 +1,37 @@
+// abspwr
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func sumDigs(n uint64) uint64 {
+	if n == 0 {
+		return 0
+	} else {
+		return n%10 + sumDigs(n/10)
+	}
+}
+
+func printDigs(n uint64) {
+	if n == 0 {
+		fmt.Println()
+	} else {
+		printDigs(n / 10)
+		fmt.Print(n % 10)
+	}
+}
+
+func main() {
+	n := 6
+	t := uint64(0x32) // 40
+	max := uint64(math.Pow10(n))
+	i := uint64(0)
+
+	for i = 1; i < max; i++ {
+		if sumDigs(i) == t {
+			printDigs(i)
+		}
+	}
+}
